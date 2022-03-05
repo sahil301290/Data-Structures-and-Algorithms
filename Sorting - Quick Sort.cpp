3cd58ef1db7fe5ded65dd8f5905ee72b5a2ab811
sahil301290 <sahil301290@users.noreply.github.com>
2022-03-05 06:55:42 +0530

    Add files via upload

---
+++ Sorting - Quick Sort.cpp
@@ -0,0 +1,59 @@
+/* 
+Quick Sort
+- Divide and Conquer.
+- Choose an element and put at right position.
+- Pivot element can be first, mid or last. However, it must stay consistent throughout.
+*/
+#include <iostream>
+using namespace std;
+
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = l-1;
+    
+    for(int j=l;j<r;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr,i,j);
+        }
+    }
+    
+    swap(arr,i+1,r);
+    return (i+1);
+}
+
+void quickSort(int arr[], int l, int r)
+{
+    if(l<r)
+    {
+        int pi = partition(arr,l,r);
+        quickSort(arr,l,pi-1);
+        quickSort(arr,pi+1,r);
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter how many elements you want to sort and enter those numbers"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    
+    quickSort(arr,0,n-1);
+    
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
